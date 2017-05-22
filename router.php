e6090ffda2af2ac84835809552ce0c8e346c066c
Olalekan Oladipupo <lakesida10@gmail.com>
2017-05-22 16:34:47 +0100

    Update router.php

--- router.php
+++ router.php
@@ -6,49 +6,53 @@ class Router {
   public function __construct(){}
   
   public function get($pattern, $callback){
-    $method =  $_SERVER['REQUEST_METHOD'];
-    if($method == 'GET'){
-      $this->routes[$pattern] = $callback;
-    }
+    $this->map("GET", $pattern, $callback);
   }
   
   public function post($pattern, $callback){
-    $method =  $_SERVER['REQUEST_METHOD'];
-    if($method == 'POST'){
-      $this->routes[$pattern] = $callback;
-    }
+    $this->map("POST", $pattern, $callback);
   }
   
   public function delete($pattern, $callback){
-    $method =  $_SERVER['REQUEST_METHOD'];
-    if($method == 'DELETE'){
-      $this->routes[$pattern] = $callback;
-    }
+    $this->map("DELETE", $pattern, $callback);
   }
   
   public function put($pattern, $callback){
-    $method =  $_SERVER['REQUEST_METHOD'];
-    if($method == 'PUT'){
-      $this->routes[$pattern] = $callback;
-    }
+    $this->map("PUT", $pattern, $callback);
   }
   
   public function map($method, $pattern, $callback){
-    //yet to be implemented
+    if($_SERVER['REQUEST_METHOD'] == $method){
+      $this->routes[$pattern] = $callback;
+    }
   
   }
   public function run($uri){
     $uri = $_GET['url'];
     foreach($this->routes as $pattern => $callback){
-      if(preg_match($pattern, $uri, $params)){
+      $route_pattern = $this->convertToRegex($route);
+      if(preg_match($route_pattern, $uri, $params)){
         array_shift($params);
         return call_user_func_array($callback, array_values($params));
       }
     }
   }
   
-  private function convertToRegex($route){
-   // yet to be implemented
+  private function convertToRegex($route) {
+    return '@^' . preg_replace_callback("@{([^}]+)}@", function ($match) {
+        return $this->regexParam($match[1]);
+    }, $route) . '$@';
   }
+  
+   private function regexParam($name) {
+      if ($name[strlen($name) - 1] == '?') {
+          $name = substr($name, 0, strlen($name) - 1);
+          $end = '?';
+      } else {
+          $end = '';
+      }
+      $pattern = isset($this->params[$name]) ? $this->params[$name] : "[^/]+";
+      return '(?<' . $name . '>' . $pattern . ')' . $end;
+    }
 }
 ?>
